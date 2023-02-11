08be407a1f671fca0d11c68bec8455a29c76957a
carsonadair <carsonadairphoto@gmail.com>
2023-02-11 14:39:02 -0500

    add uuid file and middleware

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require(`express`);
 const dbData = require(`./db/db.json`);
+const uuid = require(`./helper/uuid`)
 
 const app = express();
 const PORT = 3001
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(`public`));
 
-app.get('/', (req, res) => {
+app.get('/public/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/public/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 app.get('/api/notes', (req, res) => res.json(dbData));
